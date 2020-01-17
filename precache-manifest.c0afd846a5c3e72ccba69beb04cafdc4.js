3b3dd8d34f324b81f794b84fb9646b2767c0d88a
arshit09 <arshitvaghasiya29@gmail.com>
2020-01-17 05:30:15 +0530

    Updates

---
+++ precache-manifest.c0afd846a5c3e72ccba69beb04cafdc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f782518ae995d0b3ad5b024e5320836d",
+    "url": "/arshit09/todo-react/index.html"
+  },
+  {
+    "revision": "2ba6a476956e1f9394a4",
+    "url": "/arshit09/todo-react/static/css/main.28ed2570.chunk.css"
+  },
+  {
+    "revision": "3177df69cd13c7548234",
+    "url": "/arshit09/todo-react/static/js/2.d6c9aeed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/arshit09/todo-react/static/js/2.d6c9aeed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ba6a476956e1f9394a4",
+    "url": "/arshit09/todo-react/static/js/main.5a6d2015.chunk.js"
+  },
+  {
+    "revision": "e51c148f72596d01be19",
+    "url": "/arshit09/todo-react/static/js/runtime-main.b8b5f224.js"
+  },
+  {
+    "revision": "82a1cb0b15fdd225ab8c47f41c5a3319",
+    "url": "/arshit09/todo-react/static/media/o-letter.82a1cb0b.png"
+  },
+  {
+    "revision": "51e8a2e12cac324c552cd624a70a3d28",
+    "url": "/arshit09/todo-react/static/media/share.51e8a2e1.png"
+  }
+]);
